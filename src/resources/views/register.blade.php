63d49391191ac343e19ab86fd7f9f6c44bd720f4
ta8i2ga <ta8i2ga@outlook.com>
2024-02-02 16:31:26 +0900

    Laravel認証追加、日本語化

---
+++ src/resources/views/register.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atte</title>
+</head>
+<body>
+    <header>
+        <div class="header__inner">
+            <h1>Atte</h1>
+        </div>
+    </header>
+    <main>
+
+    </main>
+</body>
+</html>
